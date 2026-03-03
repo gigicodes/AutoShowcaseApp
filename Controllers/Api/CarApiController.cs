d5352022d0e90d8bb6231816a737ae3e65d64f7d
gigicodes <gifftchiedozie@gmail.com>
2026-03-03 16:04:41 +0100

    Created APIs for cars and inquiries. Fixed availability button in cars page, and the asp asction in admin cars page.

---
+++ Controllers/Api/CarApiController.cs
@@ -0,0 +1,41 @@
+﻿using AutoShowcaseApp.Models;
+using AutoShowcaseApp.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AutoShowcaseApp.Controllers.Api
+{
+    [Route("api/cars")]
+    [ApiController]
+    public class CarApiController : ControllerBase
+    {
+        private readonly ICarRepository _cars;
+        public CarApiController(ICarRepository cars)
+        {
+            _cars = cars;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var cars = _cars.GetAll();
+            return Ok(cars);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            var car = _cars.GetById(id);
+            if (car == null)
+                return NotFound();
+            return Ok(car);
+        }
+
+        [HttpPost]
+        public IActionResult Submit([FromBody] Car car)
+        {
+            _cars.Add(car);
+            return Ok(new { message = "Car added successfully.", id = car.Id });
+        }
+    }
+}
---
+++ Controllers/Api/InquiriesApiController.cs
@@ -0,0 +1,41 @@
+﻿using AutoShowcaseApp.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using AutoShowcaseApp.Models;
+
+namespace AutoShowcaseApp.Controllers.Api
+{
+    [Route("api/inquiries")]
+    [ApiController]
+    public class InquiriesApiController : ControllerBase
+    {
+        private readonly IInquiryRepository _inquiries;
+        public InquiriesApiController(IInquiryRepository inquiries)
+        {
+            _inquiries = inquiries;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var inquiries = _inquiries.GetAll();
+            return Ok(inquiries);
+        }
+
+        [HttpPost]
+        public IActionResult Submit([FromBody] Inquiry inquiry)
+        {
+            _inquiries.Add(inquiry);
+            return Ok(new { message = "Inquiry submitted successfully.", id = inquiry.Id });
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            var inquiry = _inquiries.GetById(id);
+            if (inquiry == null)
+                return NotFound();
+            return Ok(inquiry);
+        }
+    }
+}
